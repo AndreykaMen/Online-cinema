f0aaf4a27dbc4f6b1bc53e08e3fbd50d4ea66714
AndreykaMen <aodinmg@gmail.com>
2022-05-13 19:26:10 +0600

    В файле  auth.service.ts функцию returnUserFields() сделал синхронной, иначе не получал данные пользователя

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -60,7 +60,7 @@ export class AuthService {
     return { refreshToken, accessToken };
   }
 
-  async returnUserFields(user: UserModel) {
+  returnUserFields(user: UserModel) {
     return {
       _id: user._id,
       email: user.email,
