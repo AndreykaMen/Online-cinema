19ad2b4f3cac326ac6ca460dd50d0fccda5435ca
AndreykaMen <aodinmg@gmail.com>
2022-05-14 23:34:57 +0600

    Создал кастомные декораторы и поиск по id,с возвратом инфы о регистрированом пользователе

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { UserModel } from "./user.model";
 import { ModelType } from "@typegoose/typegoose/lib/types";
 import { InjectModel } from "nestjs-typegoose";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>){}
-  async byId(){
-    return {email: 'testByID@test.com'}
+  constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>) {
+  }
+
+  async byId(_id: string) {
+    const user = await this.userModel.findById(_id);
+
+    if (!user) throw new NotFoundException("User not found");
+    return user;
+    //Либо можно возвращать выбранные поля из user
+    // return {
+    //   email: user.email,
+    //   isAdmin: user.isAdmin,
+    //   favorites: user.favorites,
+    //   createdAt: user.createdAt
+    // }
   }
 }
