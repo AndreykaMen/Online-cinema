bf1927bc71a50706200d57f90e3bba56e876113c
AndreykaMen <aodinmg@gmail.com>
2022-05-15 01:14:10 +0600

    Создал новый эндпоинт updateProfile в user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Put, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { Auth } from "../auth/decorators/auth.decorator";
 import { User } from "./decorators/user.decorator";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
-  constructor(private readonly userService: UserService){}
-
-  @Get('profile')
-  @Auth()
-  async getProfile(@User('_id') _id:string){
-    return this.userService.byId(_id)
+  constructor(private readonly userService: UserService) {
   }
+
+  @Get("profile")
+  @Auth()
+  async getProfile(@User("_id") _id: string) {
+    return this.userService.byId(_id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put("profile")
+  @HttpCode(200)
+  @Auth()
+  async updateProfile(@User("_id") _id: string, @Body() dto: UpdateUserDto) {
+    return this.userService.updateProfile(_id, dto);
+  }
+
 }
