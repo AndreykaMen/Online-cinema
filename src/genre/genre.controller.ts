70826523abeb00252a9bc787685ab60e376e1fd2
AndreykaMen <aodinmg@gmail.com>
2022-05-16 00:51:12 +0600

    в genre.controller.ts создал методы

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,4 +1,72 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common";
+import { GenreService } from "./genre.service";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { IdValidationPipe } from "../pipes/id.validation.pipe";
+import { CreateGenreDto } from "./dto/create-genre.dto";
 
-@Controller('genre')
-export class GenreController {}
+@Controller("genres")
+export class GenreController {
+  constructor(private readonly genreService: GenreService) {
+  }
+
+  @Get("by-slug/:slug")
+  @Auth()
+  async bySlug(@Param("slug") slug: string) {
+    return this.genreService.bySlug(slug);
+  }
+
+  @Get("/collections")
+  async getCollections() {
+    return this.genreService.getCollections();
+  }
+
+  @Get()
+  async getAll(@Query("searchTerm") searchTerm?: string) {
+    return this.genreService.getAll(searchTerm);
+  }
+
+  @Get(":id")
+  @Auth("admin")
+  async get(@Param("id", IdValidationPipe) id: string) {
+    return this.genreService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth("admin")
+  async create() {
+    // @ts-ignore
+    return this.genreService.create();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(":id")
+  @HttpCode(200)
+  @Auth("admin")
+  async update(
+    @Param("id", IdValidationPipe) id: string,
+    @Body() dto: CreateGenreDto
+  ) {
+    return this.genreService.update(id, dto);
+  }
+
+  @Delete(":id")
+  @HttpCode(200)
+  @Auth("admin")
+  async delete(@Param("id", IdValidationPipe) id: string) {
+    return this.genreService.delete(id);
+  }
+}
